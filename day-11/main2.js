396b5453ef15ddf216b7f6d6f194534ceeab0d85
tomeraitz <tomeraitz1@gmail.com>
2018-12-04 14:05:34 +0200

    finished mini project

--- day-11/main2.js
+++ day-11/main2.js
@@ -2,9 +2,10 @@ $.ajax({
     method: "GET",
     url: `https://jsonplaceholder.typicode.com/users`,
     success: function (data) {
-        data.forEach(d =>
-            $("#users").append(`<div class="thisUsers" id="${d.id}"><h3>${d.name}</h3><a href="${d.email}">${d.email}</a></div>`)
-          )
+        const source = $('#users-template').html();
+        let template = Handlebars.compile(source);
+        let newHTML = template({data});
+        $('#users').append(newHTML);
     },
     error: function (xhr, text, error) {
         console.log(text)
